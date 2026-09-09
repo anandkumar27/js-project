d9d21f7f533224c2bb492b6a1b0e2166e0faca11
Anand kumar <anandkumar@Anands-MacBook-Air.local>
2026-09-09 09:26:57 +0530

    2 projects and setTimeOut

---
+++ 08_Events/Sync_and_asynch.js
@@ -0,0 +1,12 @@
+console.log("First");
+
+// setTimeout(function() {
+//     console.log("Second");
+// }, 3000);
+
+// console.log("Third");
+
+
+setInterval(() => {
+    console.log("Second");
+},3000)
